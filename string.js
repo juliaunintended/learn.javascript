85c0d08985b4c223b8d37875453669a2ac66d732
juliaunintended <jtalanova17@gmail.com>
2017-06-28 00:10:22 +0300

    update first exercise in string.js

--- string.js
+++ string.js
@@ -3,11 +3,27 @@
 ucFirst("вася") == "Вася";
 ucFirst("") == ""; // нет ошибок при пустой строке
 
-P.S. В JavaScript нет встроенного метода для этого. Создайте функцию, используя toUpperCase() и charAt().
+P.S. В JavaScript нет встроенного метода для этого.
+Создайте функцию, используя toUpperCase() и charAt().
 
 function ucFirst(str) {
-    if str = ('') {
-        return str.toUpperCase.charAt();
+    if !(str) {
+        return str
+    } else {
+        return str[0].toUpperCase() + str.slice(1);
     }
-    
 }
+
+alert (ucFirst('julie'));
+
+/*Напишите функцию checkSpam(str), которая возвращает true, 
+если строка str содержит „viagra“ или „XXX“, а иначе false.
+Функция должна быть нечувствительна к регистру:
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false*/
+
+/*function checkSpam(str) {
+    
+}*/
