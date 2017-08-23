b1900347ff4915dcfbdc59d1efe011d40f9e81f0
juliaunintended <jtalanova17@gmail.com>
2017-08-23 16:39:44 +0300

    update solved exercise in object-conversion.js

---
+++ object-conversion.js
@@ -0,0 +1,29 @@
+/* Напишите функцию sum, которая будет работать так:
+
+sum(1)(2) == 3; // 1 + 2
+sum(1)(2)(3) == 6; // 1 + 2 + 3
+sum(5)(-1)(2) == 6
+sum(6)(-1)(-2)(-3) == 0
+sum(0)(1)(2)(3)(4)(5) == 15
+
+Количество скобок может быть любым. */
+function sum(a) {
+    var currentSum = a;
+
+    function la(b) {
+        currentSum += b;
+        return la;
+    }
+
+    la.toString = function () {
+        return currentSum;
+    };
+
+    return la;
+}
+
+alert(sum(1)(2));
+alert(sum(1)(2)(3));
+alert(sum(5)(-1)(2));
+alert(sum(6)(-1)(-2)(-3));
+alert(sum(0)(1)(2)(3)(4)(5));
